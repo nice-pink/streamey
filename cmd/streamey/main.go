ea6f7a6c132bab9446ddaf8d5ad75f6a007946d8
nice-pink <r@nice.pink>
2024-02-13 10:52:29 +0100

    Add streamey.

--- cmd/streamey/main.go
+++ cmd/streamey/main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
+	"io"
 	"os"
 	"sync"
-	"time"
 
-	"github.com/nice-pink/goutil/pkg/filesystem"
 	"github.com/nice-pink/goutil/pkg/log"
-	"github.com/nice-pink/streamey/pkg/miniomanager"
-	"github.com/nice-pink/streamey/pkg/readey"
+	"github.com/nice-pink/streamey/pkg/streamey"
 )
 
 var wg sync.WaitGroup
@@ -22,85 +19,25 @@ const (
 )
 
 func main() {
-	log.Info("--- Start readey ---")
+	log.Info("--- Start streamey ---")
 	log.Time()
 
 	// flags
-	url := flag.String("url", "", "Stream url")
-	maxBytes := flag.Int64("maxBytes", -1, "[Optional] Max bytes to read from url.")
-	outputFilepath := flag.String("outputFilepath", "", "[Optional] Output file path, if data should be dumped to file.")
+	url := flag.String("url", "", "Destination url")
+	filepath := flag.String("filepath", "", "File to stream.")
 	reconnect := flag.Bool("reconnect", false, "[Optional] Reconnect on any interruption.")
-	minioConfig := flag.String("minioConfig", "", "[Optional] Json config file for minio. Use minio if defined.")
-	minioCleanUpAfterSec := flag.Int64("minioCleanUpAfterSec", 0, "[Optional] Cleanup minio bucket after seconds.")
+	bitrate := flag.Int64("bitrate", 0, "Send bitrate.")
 	flag.Parse()
 
-	// read stream
-	go ReadStream(*url, *maxBytes, *outputFilepath, *reconnect)
-
-	// start minio sync
-	goRoutineCounter := 1
-	if *minioConfig != "" {
-		goRoutineCounter++
-
-		go ManageMinio(*minioConfig, delay, *outputFilepath, *minioCleanUpAfterSec, *reconnect)
-	}
-
-	// wait for go routines to be done
-	wg.Add(goRoutineCounter)
-	wg.Wait()
-}
-
-// stream
-
-func ReadStream(url string, maxBytes int64, outputFilepath string, reconnect bool) {
-	readey.ReadStream(url, maxBytes, outputFilepath, reconnect)
-	wg.Done()
-}
-
-// minio
-
-func ManageMinio(configFile string, delay int64, localFolder string, minioCleanUpAfterSec int64, loop bool) {
-	// read config
-	data, err := os.ReadFile(configFile)
+	// read file
+	file, err := os.Open(*filepath)
 	if err != nil {
-		log.Error(err, "Minio config file does not exist.")
-		os.Exit(2)
+		log.Err(err, "Cannot open file.")
 	}
-
-	var config miniomanager.MinioConfig
-	err = json.Unmarshal(data, &config)
+	data, err := io.ReadAll(file)
 	if err != nil {
-		log.Error(err, "No valid minio config.")
-		os.Exit(2)
+		log.Err(err, "Cannot read file.")
 	}
 
-	useSsl := true
-	m := miniomanager.NewMinioManager(config, useSsl)
-
-	// run loop
-	duration := time.Duration(delay) * time.Second
-	for {
-		// start with the dalay
-		time.Sleep(duration)
-
-		// push
-		PushFiles(m, localFolder)
-
-		// delete
-		//day := int64(3600 * 24)
-		m.DeleteFiles(bucket, minioFolder, minioCleanUpAfterSec)
-
-		if !loop {
-			break
-		}
-	}
-	wg.Done()
-}
-
-func PushFiles(m *miniomanager.MinioManger, folder string) {
-	files := filesystem.ListFiles(folder, 0, true)
-
-	for _, file := range files {
-		m.PutFile(bucket, minioFolder, folder, file, true)
-	}
+	streamey.Stream(*url, float64(*bitrate), data, *reconnect)
 }
--- pkg/streamey/streamey.go
+++ pkg/streamey/streamey.go
@@ -7,53 +7,60 @@ import (
 	"github.com/nice-pink/goutil/pkg/log"
 )
 
-func Stream(address string, sendBitRate float64, buffer []byte) {
-	// connection
-	conn, err := net.Dial("tcp", address)
-	if err != nil {
-		log.Error(err, "Can't dial.")
-		return
-	}
-	defer conn.Close()
-
-	// variables
-	var bytesWrittenCycle int = 0
-	var bytesWrittenTotal int64 = 0
-	streamStart := time.Now().UnixNano()
-	start := time.Now().UnixNano()
-	var byteIndex int64 = 0
-	var byteSegmentSize int64 = 1024
-	bufferLen := len(buffer)
-
-	// run loop
-	var count int = 0
+func Stream(address string, sendBitRate float64, buffer []byte, reconnect bool) {
 	for {
-		if byteIndex >= int64(bufferLen) {
-			byteIndex = 0
+
+		// connection
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			log.Error(err, "Can't dial.")
+			return
 		}
-		/*
-		 * calculate our instant rate over the entire transmit
-		 * duration
-		 */
-		rate := ((float64)(bytesWrittenTotal * 8)) / ((float64)(time.Now().UnixNano()-start) / 1000000000)
+		defer conn.Close()
 
-		// compare rate
-		if rate < sendBitRate {
-			// send data
-			bytesWrittenCycle, err = conn.Write(buffer[byteIndex:min(bufferLen, count*int(byteSegmentSize))])
-			if err != nil {
-				log.Error(err, "Could not send data.")
-				break
+		// variables
+		var bytesWrittenCycle int = 0
+		var bytesWrittenTotal int64 = 0
+		streamStart := time.Now().UnixNano()
+		start := time.Now().UnixNano()
+		var byteIndex int64 = 0
+		var byteSegmentSize int64 = 1024
+		bufferLen := len(buffer)
+
+		// run loop
+		var count int = 0
+		for {
+			if byteIndex >= int64(bufferLen) {
+				byteIndex = 0
 			}
-			bytesWrittenTotal += int64(bytesWrittenCycle)
-			byteIndex += int64(bytesWrittenCycle)
+			/*
+			 * calculate our instant rate over the entire transmit
+			 * duration
+			 */
+			rate := ((float64)(bytesWrittenTotal * 8)) / ((float64)(time.Now().UnixNano()-start) / 1000000000)
 
-			count++
+			// compare rate
+			if rate < sendBitRate {
+				// send data
+				bytesWrittenCycle, err = conn.Write(buffer[byteIndex:min(bufferLen, count*int(byteSegmentSize))])
+				if err != nil {
+					log.Error(err, "Could not send data.")
+					break
+				}
+				bytesWrittenTotal += int64(bytesWrittenCycle)
+				byteIndex += int64(bytesWrittenCycle)
+
+				count++
+			}
+		}
+
+		// final log
+		streamStop := time.Now().UnixNano()
+		passed := streamStart - streamStop
+		log.Info("Stopped sending. Bytes:", bytesWrittenTotal, ". Seconds:", passed)
+
+		if !reconnect {
+			break
 		}
 	}
-
-	// final log
-	streamStop := time.Now().UnixNano()
-	passed := streamStart - streamStop
-	log.Info("Stopped sending. Bytes:", bytesWrittenTotal, ". Seconds:", passed)
 }
